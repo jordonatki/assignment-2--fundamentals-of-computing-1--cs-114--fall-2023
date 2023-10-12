b6fa4415a36576edee8af3a6f8a1cc3a2f54b253
jordonatki <143655877+jordonatki@users.noreply.github.com>
2023-10-11 21:32:28 -0400

    Initial commit

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("enter a number");
+
+
+
+
+
+
   }
 }
