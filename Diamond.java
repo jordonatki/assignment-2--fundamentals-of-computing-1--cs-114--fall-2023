90a2be0d6bab13d8617675b0df6fb14855c341b8
jordonatki <143655877+jordonatki@users.noreply.github.com>
2023-10-16 18:04:12 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -1,14 +1,30 @@
 import java.util.Scanner;
-
+//Goal is to take user input and use it print a diamond pattern using "*"
 public class Diamond {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("enter a number");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a number");
+    int row = scanner.nextInt();
+    int col = (row+1)/2;
+    int a,b,c;
 
-
-
-
-
-
+    for (a=1; a<=col; a++) { //outer loop to handle rows
+      for (b=1; (b<=col-a);b++) { //handles columns
+        System.out.print(" ");
+      }
+      for (c=1; c<=2*a-1; c++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for (a=1; a<=col; a++) { //outer loop to handles rows
+      for (b=1; b<=a; b++) { //handles columns
+        System.out.print(" ");
+      }
+      for (c=0; c<(2*(col-a)-1); c++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
